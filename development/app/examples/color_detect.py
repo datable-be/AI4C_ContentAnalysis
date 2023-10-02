625c13e21bbae49cd39e7ae1de30dfa1b3890141
Tom Deneire <tomdeneire@gmail.com>
2023-10-02 11:07:31 +0200

    (feat) add typical POST request

--- development/app/examples/color_detect.py
+++ development/app/examples/color_detect.py
@@ -1,7 +1,26 @@
 import requests
 import sys
 
-response = requests.post("http://localhost:8000/v1/color/detect", {})
+request = """
+{
+  "requestType": "color",
+  "data": {
+    "id": "http://mint-projects.image.ntua.gr/europeana-fashion/500208081",
+    "max_colors": 3,
+    "min_area": 0.15,
+    "foreground_detection": TRUE,
+    "selector" : {
+      "type" : "FragmentSelector",
+      "conformsTo" : "http://www.w3.org/TR/media-frags/",
+      "value" : "xywh=percent:87,63,9,21"
+    }
+    "source":
+"http://example.com/images/123.jpg"
+  }
+}
+"""
+
+response = requests.post("http://localhost:8000/v1", request)
 if response.status_code == 200:
     print(response.text)
 else:
--- development/app/examples/object_detect.py
+++ development/app/examples/object_detect.py
@@ -1,7 +1,22 @@
 import requests
 import sys
 
-response = requests.post("http://localhost:8000/v1/color/detect", {})
+request = """
+{
+  "requestType": "object",
+  "data": {
+    "id": "http://example.com/images/123",
+    "min_confidence": 0.8,
+    "max_objects": 1,
+    "source":
+"http://example.com/images/123.jpg",
+    "service":"GoogleVision",
+    "service_key":"****"
+  }
+}
+"""
+
+response = requests.post("http://localhost:8000/v1", request)
 if response.status_code == 200:
     print(response.text)
 else:
