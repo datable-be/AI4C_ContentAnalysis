3847a8ac6f08856f4474e5de50a7c0e2193ef506
Tom Deneire <tomdeneire@gmail.com>
2023-10-02 13:06:23 +0200

    (chore) improve example code

--- development/app/examples/color_detect.py
+++ development/app/examples/color_detect.py
@@ -1,27 +1,39 @@
+import json
 import requests
 import sys
 
-request = """
-{
+
+URL = "http://0.0.0.0:8000/v1"
+
+REQUEST = """{
   "requestType": "color",
   "data": {
     "id": "http://mint-projects.image.ntua.gr/europeana-fashion/500208081",
     "max_colors": 3,
     "min_area": 0.15,
-    "foreground_detection": TRUE,
+    "foreground_detection": true,
     "selector" : {
       "type" : "FragmentSelector",
       "conformsTo" : "http://www.w3.org/TR/media-frags/",
       "value" : "xywh=percent:87,63,9,21"
-    }
-    "source":
-"http://example.com/images/123.jpg"
+    },
+    "source": "http://example.com/images/123.jpg"
   }
 }
 """
 
-response = requests.post("http://localhost:8000/v1", request)
+
+def json_pretty_print(json_string: str):
+    print(json.dumps(json.loads(json_string), indent=4))
+
+
+print("POST", URL)
+print("REQUEST = ")
+json_pretty_print(REQUEST)
+
+response = requests.post("http://0.0.0.0:8000/v1", REQUEST)
 if response.status_code == 200:
-    print(response.text)
+    print("RESPONSE = ")
+    json_pretty_print(response.text)
 else:
     sys.exit("ERROR")
--- development/app/examples/object_detect.py
+++ development/app/examples/object_detect.py
@@ -1,23 +1,35 @@
+import json
 import requests
 import sys
 
-request = """
-{
+URL = "http://0.0.0.0:8000/v1"
+
+
+REQUEST = """{
   "requestType": "object",
   "data": {
     "id": "http://example.com/images/123",
     "min_confidence": 0.8,
     "max_objects": 1,
-    "source":
-"http://example.com/images/123.jpg",
+    "source": "http://example.com/images/123.jpg",
     "service":"GoogleVision",
     "service_key":"****"
   }
 }
 """
 
-response = requests.post("http://localhost:8000/v1", request)
+
+def json_pretty_print(json_string: str):
+    print(json.dumps(json.loads(json_string), indent=4))
+
+
+print("POST", URL)
+print("REQUEST = ")
+json_pretty_print(REQUEST)
+
+response = requests.post("http://0.0.0.0:8000/v1", REQUEST)
 if response.status_code == 200:
-    print(response.text)
+    print("RESPONSE = ")
+    json_pretty_print(response.text)
 else:
     sys.exit("ERROR")
