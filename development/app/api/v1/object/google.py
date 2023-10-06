dca06e09c8718f2d77cbbb333be2555e1b1b14bf
Tom Deneire <tomdeneire@gmail.com>
2023-10-06 10:04:22 +0200

    (doc) document request

--- development/app/api/v1/object/google.py
+++ development/app/api/v1/object/google.py
@@ -1,5 +1,12 @@
 from classes import ObjectRequest
 
 
+# ObjectRequest =
+#      "id": "http://mint-projects.image.ntua.gr/europeana-fashion/500208081",
+#      "min_confidence": 0.8,
+#      "max_objects": 1,
+#      "source": "http://example.com/images/123.jpg",
+#      "service":"GoogleVision",
+#      "service_key":"****"
 def detection(object_request: ObjectRequest, settings: dict):
     ...
