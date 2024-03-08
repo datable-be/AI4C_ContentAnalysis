797851597679e71022fad33edc401c6bf234fba5
Tom Deneire <tomdeneire@gmail.com>
2024-03-08 09:11:20 +0100

    (feat) implement foreground detection in HuggingFace

--- development/app/api/v1/color/detect.py
+++ development/app/api/v1/color/detect.py
@@ -6,59 +6,59 @@ from api.v1.color.huggingface import detection as huggingface_detection
 from classes import ColorRequest, RequestService
 
 MODEL_RESPONSE = {
-    "@context": "...",
-    "@graph": [
+    '@context': '...',
+    '@graph': [
         {
-            "id": "http://datable.be/object-annotations/123",
-            "type": "Annotation",
-            "created": "2023-09-30",
-            "creator": {
-                "id": "https://github.com/hvanstappen/AI4C_object-detector",
-                "type": "Software",
-                "name": "AI4C object detector",
+            'id': 'http://datable.be/object-annotations/123',
+            'type': 'Annotation',
+            'created': '2023-09-30',
+            'creator': {
+                'id': 'https://github.com/hvanstappen/AI4C_object-detector',
+                'type': 'Software',
+                'name': 'AI4C object detector',
             },
-            "body": {
-                "source": "http://www.wikidata.org/entity/Q200539",
-                "type": "SpecificResource",
-                "purpose": "tagging",
+            'body': {
+                'source': 'http://www.wikidata.org/entity/Q200539',
+                'type': 'SpecificResource',
+                'purpose': 'tagging',
             },
-            "target": {
-                "source": "http://example.com/image123.jpg",
-                "type": "Image",
-                "id": "http://mint-projects.image.ntua.gr/europeana-fashion/500208081",
-                "selector": {
-                    "type": "FragmentSelector",
-                    "conformsTo": "http://www.w3.org/TR/media-frags/",
-                    "value": "xywh=percent:10,20,5,14",
+            'target': {
+                'source': 'http://example.com/image123.jpg',
+                'type': 'Image',
+                'id': 'http://mint-projects.image.ntua.gr/europeana-fashion/500208081',
+                'selector': {
+                    'type': 'FragmentSelector',
+                    'conformsTo': 'http://www.w3.org/TR/media-frags/',
+                    'value': 'xywh=percent:10,20,5,14',
                 },
             },
-            "confidence": 0.8,
+            'confidence': 0.8,
         },
         {
-            "id": "http://datable.be/object-annotations/456",
-            "type": "Annotation",
-            "created": "2023-09-30",
-            "creator": {
-                "id": "https://github.com/hvanstappen/AI4C_object-detector",
-                "type": "Software",
-                "name": "AI4C object detector",
+            'id': 'http://datable.be/object-annotations/456',
+            'type': 'Annotation',
+            'created': '2023-09-30',
+            'creator': {
+                'id': 'https://github.com/hvanstappen/AI4C_object-detector',
+                'type': 'Software',
+                'name': 'AI4C object detector',
             },
-            "body": {
-                "source": "http://www.wikidata.org/entity/Q80151",
-                "type": "SpecificResource",
-                "purpose": "tagging",
+            'body': {
+                'source': 'http://www.wikidata.org/entity/Q80151',
+                'type': 'SpecificResource',
+                'purpose': 'tagging',
             },
-            "target": {
-                "source": "http://example.com/image123.jpg",
-                "type": "Image",
-                "id": "http://mint-projects.image.ntua.gr/europeana-fashion/500208081",
-                "selector": {
-                    "type": "FragmentSelector",
-                    "conformsTo": "http://www.w3.org/TR/media-frags/",
-                    "value": "xywh=percent:87,63,9,21",
+            'target': {
+                'source': 'http://example.com/image123.jpg',
+                'type': 'Image',
+                'id': 'http://mint-projects.image.ntua.gr/europeana-fashion/500208081',
+                'selector': {
+                    'type': 'FragmentSelector',
+                    'conformsTo': 'http://www.w3.org/TR/media-frags/',
+                    'value': 'xywh=percent:87,63,9,21',
                 },
             },
-            "confidence": 0.7,
+            'confidence': 0.7,
         },
     ],
 }
@@ -84,7 +84,7 @@ def detection(
     color_processor: BlipProcessor,
     settings: dict,
 ) -> dict:
-    if settings.get("dummy"):
+    if settings.get('dummy'):
         return MODEL_RESPONSE
 
     if color_request.service == RequestService.internal:
@@ -92,5 +92,5 @@ def detection(
 
     elif color_request.service == RequestService.huggingface:
         return huggingface_detection(
-            color_request, color_model, color_processor, settings
+            color_request, net, color_model, color_processor, settings
         )
--- development/app/api/v1/color/huggingface.py
+++ development/app/api/v1/color/huggingface.py
@@ -1,10 +1,11 @@
 # See https://huggingface.co/Salesforce/blip-vqa-base
 
 from PIL import Image
+from cv2.dnn import Net
 import torch
 from transformers import BlipProcessor, BlipForQuestionAnswering
 
-from api.v1.tools.image import url_to_tempfile
+from api.v1.tools.image import url_to_tempfile, determine_image
 from api.v1.tools.color import extract_colors_from_sentence, add_URIs
 
 from classes import ColorRequest
@@ -12,58 +13,69 @@ from classes import ColorRequest
 
 def detection(
     color_request: ColorRequest,
+    net: Net,
     model: BlipForQuestionAnswering,
     processor: BlipProcessor,
     settings: dict,
 ) -> dict:
     result = {}
 
+    temp_path = determine_image(color_request, net, settings, resize=None)
+    if not temp_path:
+        return result
+
     # Move the model to GPU if available
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # device = torch.device("cpu")
     model.to(device)
 
     try:
-        url = str(color_request.source)
-
-        # to do: what about color_request.foreground_detection? and min_area and such?
-        file_path = url_to_tempfile(url, None)
-        image = Image.open(file_path).convert("RGB")
+        image = Image.open(temp_path).convert('RGB')
 
         # Determine object type
         inputs_object = processor(
             image,
-            "What is the main foreground subject of the image?",
-            return_tensors="pt",
+            'What is the main foreground subject of the image?',
+            return_tensors='pt',
         ).to(device)
         out_object = model.generate(**inputs_object, max_new_tokens=20)
-        answer_object = processor.decode(out_object[0], skip_special_tokens=True)
+        answer_object = processor.decode(
+            out_object[0], skip_special_tokens=True
+        )
 
         if answer_object in [
-            "man",
-            "woman",
-            "mannequin",
-            "girl",
-            "boy",
-            "model",
-            "blonde woman",
+            'man',
+            'woman',
+            'mannequin',
+            'girl',
+            'boy',
+            'model',
+            'blonde woman',
         ]:
-            question_color = "Which colors are the clothes of the" + answer_object + "?"
-        elif answer_object in ["runway"]:
+            question_color = (
+                'Which colors are the clothes of the' + answer_object + '?'
+            )
+        elif answer_object in ['runway']:
             # Avoid color of the runway (happens with catwalk pictures)
-            question_color = "Which colors are the clothes of the model on the runway?"
+            question_color = (
+                'Which colors are the clothes of the model on the runway?'
+            )
         else:
-            question_color = "Which colors has the " + answer_object + "?"
+            question_color = 'Which colors has the ' + answer_object + '?'
 
         # Determine color
-        inputs_color = processor(image, question_color, return_tensors="pt").to(device)
+        inputs_color = processor(
+            image, question_color, return_tensors='pt'
+        ).to(device)
         out_color = model.generate(**inputs_color, max_new_tokens=20)
         answer_color = processor.decode(out_color[0], skip_special_tokens=True)
 
         colors = extract_colors_from_sentence(answer_color)
 
-        percentages = {color: None for color in colors}
-        result = add_URIs(percentages)
+        # HuggingFace does not allow to determine percentages
+        colors_with_fake_percentages = {color: None for color in colors}
+
+        result = add_URIs(colors_with_fake_percentages)
 
     except Exception as e:
         # to do (see also object error handling?)
